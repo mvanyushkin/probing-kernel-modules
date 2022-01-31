931329ce769080352f64a15e3b87fd016465dcfc
Max Vanyushkin <max.vanyushkin@gmail.com>
2022-01-31 21:29:43 +0400

    timer works

--- maxmodule.c
+++ maxmodule.c
@@ -10,19 +10,25 @@ MODULE_DESCRIPTION("Just starting kernel modules development process");
 MODULE_VERSION("1.00");
 
 
-static int __init
+static struct timer_list my_timer;
 
-lkm_example_init(void) {
+void my_timer_callback( struct timer_list *args)
+{
+    mod_timer(&my_timer, jiffies + msecs_to_jiffies(2000));
+    printk(KERN_INFO"Hello from timer!\n");
+}
+
+static int __init lkm_example_init(void) {
+    timer_setup(&my_timer, my_timer_callback, 0);
+    mod_timer(&my_timer, jiffies + msecs_to_jiffies(2000));
     printk(KERN_INFO
     "Hello, World!\n");
     return 0;
 }
 
 
-static void __exit
-
-lkm_example_exit(void) {
-    is_completed = 1;
+static void __exit lkm_example_exit(void) {
+    del_timer(&my_timer);
     printk(KERN_INFO
     "Goodbye, World!\n");
 }
@@ -31,4 +37,3 @@ lkm_example_exit(void) {
 module_init(lkm_example_init);
 module_exit(lkm_example_exit);
 
- 
