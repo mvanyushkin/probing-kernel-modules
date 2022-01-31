c398a7bc3385a8e4fceb8b3a7fde527af8102c8f
Max Vanyushkin <max.vanyushkin@gmail.com>
2022-01-31 22:51:24 +0400

    done

--- maxmodule.c
+++ maxmodule.c
@@ -4,17 +4,28 @@
 #include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Maxim V");
 MODULE_DESCRIPTION("Just starting kernel modules development process");
 MODULE_VERSION("1.00");
 
 
+char *myCustomParameter;
+
+// creates a param at
+// /sys/module/maxmodule/parameters/myCustomParameter
+module_param(myCustomParameter, charp, S_IRUSR|S_IWUSR);
+
 static int __init
 
 lkm_example_init(void) {
-    printk(KERN_INFO
-    "Hello, World!\n");
+    printk("myCustomParameter = %s \n", myCustomParameter);
+    pr_info("Hello, World!\n");
     return 0;
 }
 
@@ -22,13 +33,10 @@ lkm_example_init(void) {
 static void __exit
 
 lkm_example_exit(void) {
-    is_completed = 1;
-    printk(KERN_INFO
-    "Goodbye, World!\n");
+    printk("myCustomParameter = %s \n", myCustomParameter);
+    pr_info("Goodbye, World!\n");
 }
 
 
 module_init(lkm_example_init);
 module_exit(lkm_example_exit);
-
- 
